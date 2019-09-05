a45b5e4c1ac99e4e6d1d1a4f69e6e4a28d44464b
mohamedadel1994825 <mohamedadel1994825@gmail.com>
2019-09-05 21:32:38 +0200

    modify code

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import App1 from './App1';
+import TextPlink from './components/TextPlink';
+import TextApp from './components/TextApp';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TextApp);
